ced73aa65f7a3828eeef6b4253c2ecc4dce50a21
Hiroyuki Shoji <hiroyuki.shoji@sony.com>
2023-06-11 19:37:30 +0900

    Initial Commit

---
+++ LOTR Converter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LOTR Converter
+//
+//  Created by 小路宏幸 on 2023/06/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LOTR Converter/LOTR_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTR_ConverterApp.swift
+//  LOTR Converter
+//
+//  Created by 小路宏幸 on 2023/06/11.
+//
+
+import SwiftUI
+
+@main
+struct LOTR_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
